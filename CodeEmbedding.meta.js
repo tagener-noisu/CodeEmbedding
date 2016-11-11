27b16dee72fee669c426f5d8c994e94cfe608675
tagener-noisu <op4.renegat@gmail.com>
2016-11-11 10:20:48 +0600

    Add auto-update feature, closes #3

---
+++ CodeEmbedding.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        CodeEmbedding
+// @namespace   org.noisu
+// @description Code embedding script for imageboards
+// @include     http://dobrochan.com/*/res/*
+// @version     1
+// @grant       none
+// @updateURL https://github.com/tagener-noisu/CodeEmbedding/raw/master/CodeEmbedding.meta.js
+// @downloadURL https://github.com/tagener-noisu/CodeEmbedding/raw/master/CodeEmbedding.user.js
+// ==/UserScript==
