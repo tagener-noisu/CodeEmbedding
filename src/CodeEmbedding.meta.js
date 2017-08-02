fc1e4726e271c7d87758e4d98f2a699399104ffa
tagener-noisu <op4.renegat@gmail.com>
2017-08-02 18:12:30 +0600

    Fix meta-file

--- src/CodeEmbedding.meta.js
+++ src/CodeEmbedding.meta.js
@@ -3,8 +3,7 @@
 // @namespace   org.noisu
 // @description Code embedding script for imageboards
 // @include     http://dobrochan.com/*/res/*
+// @include	https://2ch.hk/*/res/*
 // @version     1.3.2
 // @grant       none
-// @updateURL https://github.com/tagener-noisu/CodeEmbedding/raw/master/CodeEmbedding.meta.js
-// @downloadURL https://github.com/tagener-noisu/CodeEmbedding/raw/master/CodeEmbedding.user.js
 // ==/UserScript==
