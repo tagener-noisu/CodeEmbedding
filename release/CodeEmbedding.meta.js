ea5c201ab66670f523e153430ad8a764a5fc3ae8
tagener-noisu <op4.renegat@gmail.com>
2017-08-08 18:37:47 +0600

    Add copyright notice to the meta file

--- release/CodeEmbedding.meta.js
+++ release/CodeEmbedding.meta.js
@@ -4,6 +4,21 @@
 // @description Code embedding script for imageboards
 // @include     http://dobrochan.com/*/res/*
 // @include	https://2ch.hk/*/res/*
-// @version     1.3.2
+// @version     1.3.3
 // @grant       none
 // ==/UserScript==
+
+// Copyright (C) 2016-2018 tagener-noisu <op4.renegat@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
