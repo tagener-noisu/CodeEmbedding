612d97da0c40d6f6aa8f48c5e6db81342333c1d2
tagener-noisu <op4.renegat@gmail.com>
2017-08-15 00:20:28 +0600

    Add services.ml, add pastebin service

---
+++ src/services.ml
@@ -0,0 +1,10 @@
+open Service;;
+
+let pastebin = createService
+	~selector: {j|a[href*="//pastebin.com"]|j}
+	~embed_url: (
+			fun url ->
+			match url |. regex [%bs.re "/\.com\/([A-Za-z0-9]+)$/"] with
+			| Some([|_;id|]) -> "//pastebin.com/embed_iframe/" ^ id
+			| _ -> raise UrlMatchFailure
+	)
