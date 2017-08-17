8b1d446ad059a6d3290304e75e02c65675d7fda3
tagener-noisu <op4.renegat@gmail.com>
2017-08-18 00:37:21 +0600

    Code refactoring

--- test/services_test.ml
+++ test/services_test.ml
@@ -2,21 +2,22 @@ type test
 type expected
 external describe : string -> (unit -> unit) -> unit = "" [@@bs.val]
 external it : string -> (unit -> test) -> test = "" [@@bs.val]
-external expect : 'a -> expected = "" [@@bs.val]
+external expect_ : 'a -> expected = "expect" [@@bs.val]
 external toBe : expected -> 'a -> test = "" [@@bs.send]
+
+let expect x fn y = expect_ x |. fn y
 
 open Service;;
 
 let _ = describe "pastebin" (fun _ ->
 
-let _ =
 	let result =
 	Services.pastebin |. embed_url "https://pastebin.com/Leet1337" in
 	let url = "//pastebin.com/embed_iframe/Leet1337" in
 	it "should return correct url for embedding"
-		(fun _ -> expect result |. toBe url)
-in
+		(fun _ -> expect result toBe url);
 
-()
+	it "should pass" (fun _ -> expect true toBe true);
 
+	()
 )
