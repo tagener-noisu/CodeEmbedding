783286b95f6fabab95692f06887bbc000d4970db
tagener-noisu <op4.renegat@gmail.com>
2017-09-20 17:41:19 +0600

    Implement ideone service

--- src/services.ml
+++ src/services.ml
@@ -9,5 +9,13 @@ let pastebin = createService
 			| _ -> raise UrlMatchFailure
 	)
 
+let ideone = createService
+	~selector: {j|a[href*="//ideone.com"]|j}
+	~embed_url: (
+			fun url ->
+			match url |. regex [%bs.re "/\.com\/([A-Za-z]+)$/"] with
+			| Some([|_;id|]) -> "//ideone.com/embed/" ^ id
+			| _ -> raise UrlMatchFailure
+	)
 
 let services = [|pastebin|]
